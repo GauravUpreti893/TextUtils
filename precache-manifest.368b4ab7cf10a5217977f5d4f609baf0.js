fa3158f21face18b0c30cdd722465447da3f7b35
GauravUpreti893 <gauravupreti_co20b3_70@dtu.ac.in>
2022-05-29 07:56:50 +0530

    Updates

---
+++ precache-manifest.368b4ab7cf10a5217977f5d4f609baf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb1232dd137ec7658877",
+    "url": "/TextUtils/static/css/main.aadf649f.chunk.css"
+  },
+  {
+    "revision": "fb1232dd137ec7658877",
+    "url": "/TextUtils/static/js/main.fb1232dd.chunk.js"
+  },
+  {
+    "revision": "aa1ef92c5cf7728f9d0a",
+    "url": "/TextUtils/static/js/1.aa1ef92c.chunk.js"
+  },
+  {
+    "revision": "0b832a9bb11d7e92bd08",
+    "url": "/TextUtils/static/js/2.0b832a9b.chunk.js"
+  },
+  {
+    "revision": "d4728a5b0ac474055c13",
+    "url": "/TextUtils/static/js/runtime~main.d4728a5b.js"
+  },
+  {
+    "revision": "1aca671b7ba8d255ee5690945b8a4bab",
+    "url": "/TextUtils/static/media/profileimg.1aca671b.jpg"
+  },
+  {
+    "revision": "7af448c184cee349da5619ea6e45cce0",
+    "url": "/TextUtils/index.html"
+  }
+];
